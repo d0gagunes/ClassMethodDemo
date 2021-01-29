c88f517c2112965dae9d9de7c16c53d094e76083
Doga Gunes <73892482+d0gagunes@users.noreply.github.com>
2021-01-29 19:54:40 +0300

    Add project files.

---
+++ ClassMethodDemo/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string mail { get; set; }
+    }
+}
---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+       public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + customer.Surname "succesfully added.");
+
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + customer.Surname "succesfully deleted.");
+
+        }
+        public void ListCustomers(List<Customer>customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Customer Id: " + customer.Id);
+                Console.WriteLine("Customer Name: " + customer.Name);
+                Console.WriteLine("Customer Surname: " + customer.Surname);
+                Console.WriteLine("Customer Mail: " + customer.mail);
+               
+            }
+
+        }
+        
+    }
+}
